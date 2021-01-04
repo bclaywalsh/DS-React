25e0632270b33bc209039cbd960afa1c80968464
bclaywalsh <bclaywalsh@gmail.com>
2021-01-04 12:16:21 -0500

    core SPA up and running

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
 import HomeScreen from './pages/home-screen';
 import BackstoryScreen from './pages/backstory-screen';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
 
 const App = () => {
   const PageSwitch = () => (
@@ -10,7 +13,7 @@ const App = () => {
         <Route exact path='/' component={HomeScreen} />
         {/* <Route exact path='/contact' component={ContactScreen} /> */}
         <Route exact path='/backstory' component={BackstoryScreen} />
-        <Route exact path='/loadout' component={Loadout} />
+        {/* <Route exact path='/loadout' component={Loadout} /> */}
     </Switch>
   )
 
