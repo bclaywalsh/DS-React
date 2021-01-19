c89de1bf3624969a1dabfd7402036bdc6c860b6e
bclaywalsh <bclaywalsh@gmail.com>
2021-01-19 17:16:15 -0500

    better panel texture

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import style from './style.module.scss';
+import dsPanel1 from './ds-react-panel1.png';
+import greenOff from './greenOff.png';
+import greenOn from './greenOn.png';
+import redOff from './redOff.png';
+import redOn from './redOn.png';
+
 
 
 const Header = () => {
     return (
         <div className="header">
             {/* <img  className="header-back" href=""/> */}
-            <div className={style.backPanel1}></div>
+            <img className={style.panel} src={dsPanel1} alt="Derelict Star (logo)" />
+            <img className={style.lightButton} src={greenOff} alt="Derelict Star (logo)" />
+            <img className={style.lightButton} src={greenOn} alt="Derelict Star (logo)" />
+            <img className={style.lightButton} src={redOff} alt="Derelict Star (logo)" />
+            <img className={style.lightButton} src={redOn} alt="Derelict Star (logo)" />
+
             <div className={style.backPanel2}></div>
 
             <div className={style.hamburger1}></div>
