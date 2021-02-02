7567fa9f0d439b106f12a43f77f8045cb40483c2
bclaywalsh <bclaywalsh@gmail.com>
2021-02-02 17:27:02 -0500

    some history added

--- src/pages/backstory-screen/index.js
+++ src/pages/backstory-screen/index.js
@@ -6,6 +6,19 @@ const BackstoryScreen = () => {
     return (
         <div className={style.body}>
             <h1>A Collapsing Empire</h1>
+            <article>  
+                <li>-3,080 : Mankind expands beyond the borders of their home system</li>
+                <li>-2,906 : Mankind encounters their first M class garden planet, naming it Eden. Eden is inhabited by a race of giant invertebrates which was declared to be non-sentient and soon eradicated.</li>
+                <li>-2,790 : Mankind is challenged by the Corax, a species who had long traded with the Edonites and viewed them with religious awe for the pristine state of their world.</li>
+                <li>-2,688 : The reagency of Western galactic territories enfolds humanity into the community with the hopes of resolving their war with the Corax</li>
+                <li>-2,159 : The Corax are defeated and annexed to their home system, in which they soon starve and become extinct.</li> 
+                <li>-1,800 : Humanity expands their terrotories exponentially. Through trade negotiations and war they become the dominant power in the galaxy.</li>
+                <li>-1,595 : Strange larval organisms are discovered on several outlying human colony worlds. They possess a strange form of highly-mutable DNA which can metabolize highly dangerous forms of radiation. These beings, labeled as Xen-Seti, are quickly discovered to be highly useful in transforming inhospitable worlds into highly-profitable meccas of technological wonders.</li>
+                <li>-1,383 : Xen-Seti laced colonial worlds one-by-one begin going dark. All human life on these worlds vanishes.</li>
+                <li>-1,209 : The Xen-Seti begin appearing on worlds in which they were previously not present. War is declared on the threat. The reagency offers no aid.</li>
+                <li>-1,030 : Human dominance over the galaxy is broken. Humans begin retracting deeper and deeper into their own territory, but always the Xen-Seti appear.</li>
+                <li>-670 : All trace of human life dissapears.</li>
+            </article>
         </div>
     )
 }
